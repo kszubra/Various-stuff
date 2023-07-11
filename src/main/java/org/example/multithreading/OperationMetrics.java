14e319e83b2a27b1422df372d42c696d49c04829
Kacper <kcpszubra@gmail.com>
2023-07-11 12:20:23 +0200

    multthreading lessons

---
+++ src/main/java/org/example/multithreading/OperationMetrics.java
@@ -0,0 +1,82 @@
+package org.example.multithreading;
+
+import java.util.Random;
+
+public class OperationMetrics {
+
+
+    public static void main(String[] args) {
+        Metrics metrics = new Metrics();
+
+        SomeLogic logic1 = new SomeLogic(metrics);
+        SomeLogic logic2 = new SomeLogic(metrics);
+        Printer printer = new Printer(metrics);
+
+        logic1.start();
+        logic2.start();
+        printer.start();
+    }
+
+
+
+    public static class Metrics {
+        private long count = 0;
+        private volatile double average = 0.0;
+
+        public void addSample(long sample) {
+            double currentSum = average * count;
+            count++;
+            average = (currentSum + sample) / count; //volatile guarantees this line is atomic
+        }
+
+        public double getAverage() {
+            return average; //volatile guarantees this line is atomic
+        }
+    }
+
+    public static class Printer extends Thread {
+        private Metrics metrics;
+
+        public Printer(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            long start = System.currentTimeMillis();
+
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+
+                double currentAverage = metrics.getAverage();
+                System.out.println("Current average is: " + currentAverage);
+            }
+        }
+    }
+    public static class SomeLogic extends Thread {
+        private Metrics metrics;
+        private Random random = new Random();
+
+        public SomeLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            long start = System.currentTimeMillis();
+
+            while (true) {
+                try {
+                    Thread.sleep(random.nextInt(10));
+                } catch (InterruptedException e) {
+                }
+
+                long end = System.currentTimeMillis();
+                metrics.addSample(end - start);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/multithreading/SynchronizedBlocks.java
@@ -0,0 +1,94 @@
+package org.example.multithreading;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class SynchronizedBlocks {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        SynchronizedInventoryCounter syncCounter = new SynchronizedInventoryCounter();
+        AsynchronizedInventoryCounter asyncCounter = new AsynchronizedInventoryCounter();
+
+        IncrementingThread syncIncrementThread = new IncrementingThread(syncCounter);
+        DecrementingThread syncDecrementThread = new DecrementingThread(syncCounter);
+
+        IncrementingThread asyncIncrementThread = new IncrementingThread(asyncCounter);
+        DecrementingThread asyncDecrementThread = new DecrementingThread(asyncCounter);
+
+        syncIncrementThread.start();
+        syncDecrementThread.start();
+        asyncIncrementThread.start();
+        asyncDecrementThread.start();
+        syncIncrementThread.join();
+        syncDecrementThread.join();
+        asyncIncrementThread.join();
+        asyncDecrementThread.join();
+
+        System.out.println("Current synchronized items count: " + syncCounter.getItems());
+        System.out.println("Current not synchronized items count: " + asyncCounter.getItems());
+    }
+    @AllArgsConstructor
+    public static class DecrementingThread extends Thread {
+        private Counter counter;
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                counter.decrement();
+            }
+        }
+    }
+
+    @AllArgsConstructor
+    public static class IncrementingThread extends Thread {
+        private Counter counter;
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        }
+    }
+
+    public interface Counter {
+        void increment();
+        void decrement();
+    }
+
+    private static class AsynchronizedInventoryCounter implements Counter {
+        @Getter
+        private Integer items = 0;
+
+        public void increment() {
+            items++;
+        }
+
+        public void decrement() {
+            items--;
+        }
+    }
+    private static class SynchronizedInventoryCounter implements Counter{
+        private Integer items = 0;
+        Object lock = new Object();
+
+        public void increment() {
+            synchronized(lock) {
+                items++;
+            }
+        }
+
+        public void decrement() {
+            synchronized(lock) {
+                items--;
+            }
+        }
+
+        public Integer getItems() {
+            synchronized(lock) {
+                return items;
+            }
+        }
+    }
+}
